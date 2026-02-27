ea94cf5da9e37ce0d0b0d865767b5155a68b7191
Sanjay Shekar <sanjayshekar139@gmail.com>
2026-02-27 21:30:03 +0530

    Added Reverse String solution using two pointers

---
+++ Strings/reverse_string.c
@@ -0,0 +1,16 @@
+// LeetCode 344: Reverse String
+// Difficulty: Easy
+// Language: C
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+void reverseString(char* s, int sSize) {
+    int left = 0;
+    int right = sSize - 1;
+    while(left < right) {
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }
+}
