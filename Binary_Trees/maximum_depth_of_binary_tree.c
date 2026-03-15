b01bcc11ec2087b28df0eacca9bb1733d51efd70
Sanjay Shekar <sanjayshekar139@gmail.com>
2026-03-15 21:06:02 +0530

    Added Maximum Depth of Binary Tree solution

---
+++ Binary_Trees/maximum_depth_of_binary_tree.c
@@ -0,0 +1,12 @@
+// Time Complexity: O(n)
+// Space Complexity: O(h)
+int maxDepth(struct TreeNode* root) {
+    if(root == NULL)
+        return 0;
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+    if(left > right)
+        return left + 1;
+    else
+        return right + 1;
+}
