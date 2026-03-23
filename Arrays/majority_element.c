710a964c2c2177459eaa77935db2825f6e6335af
Sanjay Shekar <sanjayshekar139@gmail.com>
2026-03-23 21:19:33 +0530

    Added Majority Element solution

---
+++ Arrays/majority_element.c
@@ -0,0 +1,18 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+int majorityElement(int* nums, int numsSize) {
+
+    int count = 0;
+    int candidate = 0;
+    for(int i = 0; i < numsSize; i++){
+        if(count == 0){
+            candidate = nums[i];
+        }
+        if(nums[i] == candidate){
+            count++;
+        } else {
+            count--;
+        }
+    }
+    return candidate;
+}
