ab628ca7522f41663264e358ed9274cae373efd1
Sanjay Shekar <sanjayshekar139@gmail.com>
2026-03-31 20:20:28 +0530

    Added Find the Difference solution

---
+++ Arrays/find_the_difference.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <limits.h>
+
+int minDistance(int arr[], int n) {
+    int min = INT_MAX;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                if(j - i < min) {
+                    min = j - i;
+                }
+            }
+        }
+    }
+
+    if(min == INT_MAX) return -1;
+    return min;
+}
+
+int main() {
+    int arr[] = {7, 1, 3, 4, 1, 7};
+    int n = 6;
+
+    printf("%d", minDistance(arr, n));
+    return 0;
+}
