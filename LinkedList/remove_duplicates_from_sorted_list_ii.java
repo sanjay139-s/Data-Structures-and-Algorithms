a7038afa7e685f5d1fc0cf994dd6a0479456e443
Sanjay Shekar <sanjayshekar139@gmail.com>
2026-05-22 21:53:03 +0530

    Added Remove Duplicates from Sorted List II solution

---
+++ LinkedList/remove_duplicates_from_sorted_list_ii.java
@@ -0,0 +1,30 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        while(head != null){
+
+            if(head.next != null && head.val == head.next.val){
+
+                while(head.next != null &&
+                      head.val == head.next.val){
+                    head = head.next;
+                }
+
+                prev.next = head.next;
+
+            }else{
+                prev = prev.next;
+            }
+
+            head = head.next;
+        }
+
+        return dummy.next;
+    }
+}
